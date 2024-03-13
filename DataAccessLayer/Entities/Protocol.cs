3aee91a5fdeb3ad5929debe3051b483e81af93ca
Ivan Lopatin <vaneklopatin.main@gmail.com>
2024-03-13 14:56:52 +0300

    Созданы сущности, контекст в процессе создания

---
+++ DataAccessLayer/Entities/Protocol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public partial class Protocol
+    {
+        public Protocol() { }
+        public int RaceId { get; set; }
+        public int UserId { get; set; }
+        public int CarId { get; set; }
+
+        public virtual Race Race { get; set; }
+        public virtual User User { get ; set; }
+        public virtual Car Car { get; set; }
+    }
+}
---
+++ DataAccessLayer/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public enum Role
+    {
+        Admin,
+        Racer
+    }
+    public partial class User
+    {
+        public User() { }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Protocol> Protocols { get; set; }
+    }
+}
