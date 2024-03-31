1e5dfd427ea3432ea29b43e0dda73c7888e640c7
Ivan <vaneklopatin.main@gmail.com>
2024-03-31 19:45:58 +0300

    3-я готова, 4-я -- почти

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -11,7 +11,24 @@ using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-var autofacBuilder = new ContainerBuilder();
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("HasanKarting")));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+
+    });
+});
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+x.JsonSerializerOptions.ReferenceHandler =
+ReferenceHandler.IgnoreCycles);
 
 // Add services to the container.
 builder.Services.AddTransient<IDbRepository, DbRepository>();
