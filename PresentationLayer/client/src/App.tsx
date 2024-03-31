078b6dae0dc461212d95accbb480619c3c7f8799
Ivan <vaneklopatin.main@gmail.com>
2024-03-31 17:15:13 +0300

    Почти все работает

--- PresentationLayer/client/src/App.tsx
+++ PresentationLayer/client/src/App.tsx
@@ -1,25 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cars from './Components/Cars/Cars';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<h3>Картинг-центр "Hasan Karting"</h3>}/>
+        <Route path='/cars' element={<Cars/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
